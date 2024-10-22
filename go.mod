ed5204c827f41f3403bc608b1c00871ffc6736cc
janisgeegee@gmail.com <janisgeegee@gmail.com>
2024-10-22 15:44:17 +0800

    Created http.Server struct and started the server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Janisgee/chirpy.git
+
+go 1.23.1
--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello World!")
+	// Create an empty servemux
+	mux := http.NewServeMux()
+	svr := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	svr.ListenAndServe()
 }
